c554cb939f2f5cdd4ac2efd087b2537558df0bce
CurrotRox <richardjsmith0671@gmail.com>
2024-08-10 19:38:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         Random rand = new Random();
+
+
         int rollAmount = 1000000000; // Amount of rolls to be calculated
+        int maxThreads = 16; //Amount of threads to use in the calculation. If you have less than 16 threads in your CPU, probably change this.
 
         //Setting up a thread pool with a set amount of threads.
-        ExecutorService executor = Executors.newFixedThreadPool(28);
+        ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
         RollInstance maxOnesInstance = new RollInstance(rand);
 
         for (int i = 0; i < rollAmount; i++) {
@@ -19,31 +22,33 @@ public class Main {
         executor.shutdown();
         while (!executor.isTerminated()) {
             try {
-                Thread.sleep(25); //Waiting while the executor shuts down.
+                Thread.sleep(25); //Waiting while the executor shuts down. This might give you a busyWait warning. This is technically true, but annoying.
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
 
         long endTime = System.nanoTime();
-        long totalDuration = (endTime - startTime) / 1000000;
+        long totalDuration = (endTime - startTime) / 1000000000;
 
         System.out.println("Max ones: " + RollInstance.getMaxOnes());
         System.out.println("Completed Instances: " + RollInstance.getRunInstances());
-        System.out.println("Total duration (ms): " + totalDuration);
+        System.out.println("Total duration (Sec): " + totalDuration);
     }
 }
 
 class RollInstance implements Runnable {
     //Using AtomicIntegers to prevent race conditions of using multiple threads. The runInstances is just to make sure everything is synchronized properly.
-    private static AtomicInteger globalMaxOnes = new AtomicInteger(0);
-    private static AtomicInteger runInstances = new AtomicInteger(0);
+    private static final AtomicInteger globalMaxOnes = new AtomicInteger(0);
+    private static final AtomicInteger runInstances = new AtomicInteger(0);
 
-    private  Random rand;
+    private final Random rand;
 
     public RollInstance(Random randomGen) {
         rand = randomGen; // Passing the same instance of the rand variable, so it's not being reinitialized literally a billion times.
     }
+
+    @Override
     public void run() {
         int rolls = 0;
         for (int i = 0; i < 231; i++) {
